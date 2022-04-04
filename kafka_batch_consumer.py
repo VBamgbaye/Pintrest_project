1e29f3e4dd8f7c6aa0d6ad06d368b2a90d17ee37
VBamgbaye <bamgbayev@gmail.com>
2022-04-04 14:49:26 +0100

    new changes

--- kafka_batch_consumer.py
+++ kafka_batch_consumer.py
@@ -1,6 +1,14 @@
 from kafka import KafkaConsumer
+from json import loads
 
 TOPIC_NAME = 'MyFirstKafkaTopic'
-consumer = KafkaConsumer(TOPIC_NAME)
+consumer = KafkaConsumer(TOPIC_NAME, bootstrap_servers=['localhost:9092'],
+                         auto_offset_reset='earliest',
+                         enable_auto_commit=True,
+                         group_id='pin-interest',
+                         value_deserializer=lambda x: loads(x.decode('utf-8')))
 for message in consumer:
+    message = message.value
+    # collection.insert_one(message)
+    # print('{} added to {}'.format(message, collection))
     print(message)
